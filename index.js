4e9b894e5de3edcb16a57dbd853149256d9bd603
Alaa Jerbi <mralaajerbi@gmail.com>
2020-10-23 14:25:50 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const os = require('os')
+const express = require('express')
+const app = express()
+const port = 3000
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+app.get('/', (req, res) => {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+
+  res.render(__dirname + '/public/index', {
+      freememory_bytes:  freeMemory,
+      freememory_formatted: formatBytes(freeMemory),
+      totalmemory_bytes: totalMemory,
+      totalmemory_formatted: formatBytes(totalMemory),
+      usedmemory_bytes: usedMemory,
+      usedmemory_formatted: formatBytes(usedMemory)
+  });
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+})
